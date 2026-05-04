2e93f89e070671147691334febdb79e722820b07
Negolas8 <nicolas.constantinov.nogueira@escola.pr.gov.br>
2026-05-04 11:52:59 +0000

    versao2

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,13 @@
+const cardapio = [
+    {nome: "X-burguer", preco: 18.90, categoria: "lanche"}
+    {nome: "Batata-frita", preco: 9.50 categoria: "acompanhamento"}
+    {nome: "Suco de laranja", preco: 7.00, categoria: "bebida"},
+    {nome: "Milksheka", preco: 12.00, categoria: "sobremesa"}
+];
+console.loq(cardapio); //Impressão dos valores informados
 
+const pedido = [0];
+
+console.log(cardapio[0].nome);
+console.log(cardapio[1].preco) //preço da batata
+console.log(cardapio[2].categoria) //categoria do suco de laranja
