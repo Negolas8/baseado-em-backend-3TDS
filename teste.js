b5ebdc407a93787d960f2c410e60bb6e17e5bcdc
Negolas8 <nicolas.constantinov.nogueira@escola.pr.gov.br>
2026-05-12 11:56:00 +0000

    versão4

---
+++ teste.js
@@ -0,0 +1,13 @@
+const destino = "Foz do Iguaçu";
+const origem = "Curitiba";
+const nomeViajante = "Arthur";
+let duracao = 8;
+let transporte = "Carro";
+
+//Declarar 5 varíaveis para cadastrar alunos
+
+const jogador = "Felipe Da Silva";
+const nomeJogador = "Ice";
+const Time: "Hunters";
+const Jogo: "Apex",
+
